afb825308580b35e97ceeae527a576bbfadbb77c
Craig Ambrose <craig@craigambrose.com>
2009-03-24 22:18:02 +1300

    removed pliki simple page from this repository

--- vendor/plugins/pliki_simple_page/app/models/simple_page.rb
+++
@@ -1,9 +0,0 @@
-#   t.text :body
-#   t.integer :plugin_instance_id
-
-class SimplePage < ActiveRecord::Base
-  validates_presence_of :body, :plugin_instance_id
-  
-  acts_as_textiled :body
-  
-end
--- vendor/plugins/pliki_simple_page/db/migrate/20090314081447_add_versioning_to_simple_pages.rb
+++
@@ -1,9 +0,0 @@
-class AddVersioningToSimplePages < ActiveRecord::Migration
-  def self.up
-    add_column :simple_pages, :version, :integer
-  end
-
-  def self.down
-    remove_column :simple_pages, :version
-  end
-end
